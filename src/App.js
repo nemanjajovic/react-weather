dff74d08174e7292a9e8512bc976fc800b027304
DELL\necoj <nemanja.jovic195@gmail.com>
2022-06-20 20:54:29 +0200

    Added the app HTML frame

--- src/App.js
+++ src/App.js
@@ -1,10 +1,38 @@
+import axios from "axios";
+import { useState } from "react";
+
 import "./App.css";
 
 function App() {
+  // const apiKey = '6e75a0730264c2386f68ef0d04cad813'
+  // const url = `https://api.openweathermap.org/data/2.5/weather?q=dallas&appid=${apiKey}`
+
   return (
-    <div className="App">
-      <h1>React weather</h1>
-      <p>Firebase automatic deploy</p>
+    <div className="app">
+      <div className="container">
+        <div className="top">
+          <div className="location">
+            <p>Dallas</p>
+          </div>
+          <div className="temp">
+            <h1>65°F</h1>
+          </div>
+          <div className="description">
+            <p>Clouds</p>
+          </div>
+        </div>
+        <div className="bottom">
+          <div className="feels">
+            <p>65°F</p>
+          </div>
+          <div className="humidity">
+            <p>20%</p>
+          </div>
+          <div className="wind">
+            <p>12 MPH</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
