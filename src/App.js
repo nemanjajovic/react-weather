1d39a57bfbe9724993aac9bb9fcdeda8e6b790cb
DELL\necoj <nemanja.jovic195@gmail.com>
2022-06-22 19:16:32 +0200

    Created Header component to encapsulate code repetition in header

--- src/App.js
+++ src/App.js
@@ -1,26 +1,12 @@
 import React from "react";
 import { Routes, Route, Link } from "react-router-dom";
 
-import { useStateContext } from "./context/ContextProvider";
-
 import CurrentWeather from "./pages/CurrentWeather";
 import WeekForecast from "./pages/WeekForecast";
 
 function App() {
-  const { inputRef, location, setLocation, searchLocation } = useStateContext();
-
   return (
     <div className="app">
-      <div className="search">
-        <input
-          type="text"
-          value={location}
-          onChange={(e) => setLocation(e.target.value)}
-          onKeyDown={searchLocation}
-          placeholder="Enter location"
-          ref={inputRef}
-        />
-      </div>
       <div className="links">
         <Link className="link-item" to={"/"}>
           Today's forecast
