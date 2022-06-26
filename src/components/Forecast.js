2628a02f9d8700a442c4c23f875a3c46c8329764
DELL\necoj <nemanja.jovic195@gmail.com>
2022-06-26 13:20:49 +0200

    Added relative 5 days algorithm

--- src/components/Forecast.js
+++ src/components/Forecast.js
@@ -5,12 +5,41 @@ import { useStateContext } from "../context/ContextProvider";
 const Forecast = () => {
   const { forecast } = useStateContext();
 
+  // Get next five days relative to today algorithm START
+  const today = new Date().getDay();
+
+  const dayList = [
+    "Nedelja",
+    "Ponedeljak",
+    "Utorak",
+    "Srijeda",
+    "Cetvrtak",
+    "Petak",
+    "Subota",
+  ];
+
+  let relativeFiveDays = [];
+
+  let i = today + 1;
+
+  for (let counter = 0; counter < 5; counter++) {
+    // console.log(dayList[today + i]);
+    relativeFiveDays.push(dayList[today + i]);
+    i++;
+    if (i > 6) {
+      i = 0;
+    }
+  }
+
+  // END
+
   return (
     <>
       <div className="list">
         {forecast.map((item, index) => (
           <div className="list-items" key={index}>
-            <p>{`${item.main.temp.toFixed()}°C`}</p>
+            <p className="item">{relativeFiveDays[index]}</p>
+            <p className="item">{`${item.main.temp.toFixed()}°C`}</p>
             <img
               src={`https://openweathermap.org/img/wn/${item.weather[0].icon}.png`}
               alt="icon"
